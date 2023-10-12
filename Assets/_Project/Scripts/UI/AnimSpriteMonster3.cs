50c1c30cd08f476c129a28c464f4d19467fcf142
hoang <hoangndgcs18383@gmail.com>
2023-10-12 23:08:39 +0700

    up

--- Assets/_Project/Scripts/UI/AnimSpriteMonster3.cs
+++ Assets/_Project/Scripts/UI/AnimSpriteMonster3.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Match_3
 {
@@ -11,6 +12,7 @@ namespace Match_3
     {
         [SerializeField] private float duration;
         [SerializeField] private float offsetY;
+        [SerializeField] private Button button;
 
         private RectTransform _rectTransform;
         private Sequence _sequence;
@@ -18,7 +20,25 @@ namespace Match_3
 
         private void Start()
         {
+            button.onClick.AddListener(OnButtonClicked);
+            button.interactable = false;
             PlayAnim();
+        }
+
+        private void OnButtonClicked()
+        {
+            button.interactable = false;
+            _rectTransform.DOScale(Vector3.zero, 0.5f).OnComplete(() =>
+            {
+                _rectTransform.DOScale(Vector3.one, 0.5f);
+            });
+            _sequence.Kill();
+            _rectTransform.DOKill();
+            _rectTransform.DOLocalMoveY(_originPos.y, duration);
+            _rectTransform.DOLocalRotate(new Vector3(0, 0, 0), 0.5f);
+            
+            float randomPlay = UnityEngine.Random.Range(20f, 30f);
+            Invoke(nameof(PlayAnim), randomPlay);
         }
 
         [Button]
@@ -32,9 +52,17 @@ namespace Match_3
             _sequence = DOTween.Sequence();
             _sequence.Append(_rectTransform.DOLocalMoveY(offsetY, duration).SetRelative(true));
             _rectTransform.DOLocalRotate(new Vector3(0, 0, 2), 1).SetLoops(-1, LoopType.Yoyo);
+            button.interactable = false;
+            _sequence.OnComplete(() =>
+            {
+                button.interactable = true;
+            });
             /*_sequence.Append(_rectTransform.DOLocalMoveX(-_originPos.x, duration));
             _sequence.Append(_rectTransform.DOLocalMoveX(_originPos.x, duration));
             _sequence.Append(_rectTransform.DOLocalMoveY(-offsetY, duration).SetRelative(true));*/
         }
+        
+        
+        
     }
 }
